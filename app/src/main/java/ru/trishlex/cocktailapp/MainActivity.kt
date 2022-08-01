b665335b2a6889d9647dab430efbf22cb535ac71
trishLEX <alexeytr5@gmail.com>
2022-08-02 00:18:28 +0300

    add side bar menu

--- app/src/main/java/ru/trishlex/cocktailapp/MainActivity.kt
+++ app/src/main/java/ru/trishlex/cocktailapp/MainActivity.kt
@@ -2,18 +2,22 @@ package ru.trishlex.cocktailapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
-import android.widget.PopupMenu
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import ru.trishlex.cocktailapp.cocktail.menu.MyCocktailsActivity
 import ru.trishlex.cocktailapp.ingredient.menu.MyIngredientsActivity
 import ru.trishlex.cocktailapp.ingredient.shoplist.ShopListActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     companion object {
         private val TAB_NAMES = mapOf(0 to "Коктейли", 1 to "Ингредиенты")
@@ -22,6 +26,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawableLayout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+
+        drawerLayout.addDrawerListener(toggle)
+
+        val navView = findViewById<NavigationView>(R.id.navView)
+        navView.setNavigationItemSelectedListener(this)
+
+        toggle.isDrawerIndicatorEnabled = true
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
 
         val tabLayout = findViewById<TabLayout>(R.id.tabs)
         val viewPager = findViewById<ViewPager2>(R.id.view_pager)
@@ -54,39 +71,31 @@ class MainActivity : AppCompatActivity() {
         tab.text = TAB_NAMES[position]
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu) {
-            showMenu()
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
         }
         return super.onOptionsItemSelected(item)
     }
 
-    private fun showMenu() {
-        val popupMenu = PopupMenu(this, findViewById(R.id.menu))
-        popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
-
-        popupMenu.setOnMenuItemClickListener { item ->
-            when (item.itemId) {
-                R.id.myIngredients -> {
-                    startActivity(Intent(baseContext, MyIngredientsActivity::class.java))
-                    true
-                }
-                R.id.myCocktails -> {
-                    startActivity(Intent(baseContext, MyCocktailsActivity::class.java))
-                    true
-                }
-                R.id.myShopList -> {
-                    startActivity(Intent(baseContext, ShopListActivity::class.java))
-                    true
-                }
-                else -> false
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.myIngredients -> {
+                startActivity(Intent(baseContext, MyIngredientsActivity::class.java))
+                drawerLayout.close()
+                true
             }
+            R.id.myCocktails -> {
+                startActivity(Intent(baseContext, MyCocktailsActivity::class.java))
+                drawerLayout.close()
+                true
+            }
+            R.id.myShopList -> {
+                startActivity(Intent(baseContext, ShopListActivity::class.java))
+                drawerLayout.close()
+                true
+            }
+            else -> false
         }
-        popupMenu.show()
     }
 }
